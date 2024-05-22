e3bb0c2f48da2c79e280ffd2aa4cfc255b8caa44
imissr <khaledlovmnawie@hotmail.com>
2024-05-22 14:50:03 +0200

    entry added

--- src/main/entry/entry.java
+++ src/main/entry/entry.java
@@ -1,3 +1,19 @@
 public class entry {
 
+    String title;
+    String userName;
+    String password;
+    int id;
+
+    entry(String title,String userName,String password,int id){
+        this.password = password;
+        this.userName = userName;
+        this.title = title;
+        this.id=id;
+
+    }
+
+
+
+
 }
