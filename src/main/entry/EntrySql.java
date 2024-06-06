9dcbba7578532599a7ea71daeaa2c78e9e4c9314
imissr <khaledlovmnawie@hotmail.com>
2024-06-06 12:49:03 +0200

    removed try from sql

--- src/main/entry/EntrySql.java
+++ src/main/entry/EntrySql.java
@@ -16,13 +16,14 @@ public class EntrySql {
 
 
     public final void connect() {
-         connection = null;
+        connection = null;
         try {
             connection = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             this.close();
         }
+
 
 
     }
@@ -34,22 +35,21 @@ public class EntrySql {
 
 
     public void insert(String username, String password, String email, String title) throws SQLException {
-        String insertSQL = "INSERT INTO " + username + "(id,title,username,password) VALUES(?,?,?,?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(insertSQL)) {
+        PreparedStatement pstmt = connection.prepareStatement("INSERT INTO " + username + "(id,title,username,password) VALUES(?,?,?,?)");
             pstmt.setInt(1, id);
             pstmt.setString(2, title);
             pstmt.setString(3, email);
             pstmt.setString(4, password);
             pstmt.executeUpdate();
 
-        }
+
         id++;
     }
 
     public void delete(String username, int id) throws SQLException {
-        String deleteQUERY = "DELETE FROM " + username + " WHERE id = ?";
 
-        try (PreparedStatement pstmt = connection.prepareStatement(deleteQUERY)) {
+
+        PreparedStatement pstmt = connection.prepareStatement("DELETE FROM " + username + " WHERE id = ?");
             pstmt.setInt(1, id);
             int rowsAffected = pstmt.executeUpdate();
             if (rowsAffected > 0) {
@@ -61,26 +61,26 @@ public class EntrySql {
 
             }
 
-        }
+
     }
 
 
     public void updateId(String username) throws SQLException {
         String fetchID = "SELECT id FROM " + username + " ORDER BY id";
 
-        try (PreparedStatement fetch = connection.prepareStatement(fetchID);
-             ResultSet rs = fetch.executeQuery()) {
+        PreparedStatement fetch = connection.prepareStatement(fetchID);
+             ResultSet rs = fetch.executeQuery();
             int newID = 1;
             while (rs.next()) {
                 int currentID = rs.getInt("id");
 
 
                 String updateSqlID = "UPDATE " + username + " SET id = ? WHERE id = ?";
-                try (PreparedStatement update = connection.prepareStatement(updateSqlID)) {
+                PreparedStatement update = connection.prepareStatement(updateSqlID);
                     update.setInt(1, newID);
                     update.setInt(2, currentID);
                     update.executeUpdate();
-                }
+
                 newID++;
 
             }
@@ -91,18 +91,18 @@ public class EntrySql {
             System.out.println(" id updated seccessfully");
 
         }
-    }
+
 
     public boolean checkIfTableIsClear(String username) throws SQLException {
-        String query = "SELECT COUNT(*) AS rowcount FROM " + username;
 
-        try (PreparedStatement pstmt = connection.prepareStatement(query);
-             ResultSet rs = pstmt.executeQuery()) {
+
+        PreparedStatement pstmt = connection.prepareStatement( "SELECT COUNT(*) AS rowcount FROM " + username);
+             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 int count = rs.getInt("rowcount");
                 return count == 0;
             }
-        }
+
 
 
         return false;
@@ -112,26 +112,23 @@ public class EntrySql {
     public void returnAllEntry(String username) throws SQLException {
         String query = "SELECT id,title FROM " + username;
 
-        try (PreparedStatement pstmt = connection.prepareStatement(query);
-             ResultSet rst = pstmt.executeQuery()) {
-            System.out.printf("%-10s %-30s%n", "ID", "Title");
-            while (rst.next()) {
-                int id = rst.getInt("id");
-                String title = rst.getString("title");
-                System.out.printf("%-10d %-30s%n", id, title);
-                System.out.println("");
+    PreparedStatement pstmt = connection.prepareStatement(query);
+         ResultSet rst = pstmt.executeQuery();
+        System.out.printf("%-10s %-30s%n", "ID", "Title");
+        while (rst.next()) {
+            int id = rst.getInt("id");
+            String title = rst.getString("title");
+            System.out.printf("%-10d %-30s%n", id, title);
+            System.out.println("");
 
-
-            }
         }
     }
 
     public void returnPasswordAndUsername(String username,int id) throws SQLException {
-        String query = "SELECT username,password FROM " + username + " WHERE id = ?";
 
-        try (PreparedStatement stmt = connection.prepareStatement(query)){
+        PreparedStatement stmt = connection.prepareStatement("SELECT username,password FROM " + username + " WHERE id = ?");
             stmt.setInt(1, id);
-            try (ResultSet rs = stmt.executeQuery()) {
+          ResultSet rs = stmt.executeQuery();
                 if (rs.next()) {
                     String password = rs.getString("password");
                     String email = rs.getString("username");
@@ -139,25 +136,25 @@ public class EntrySql {
                 } else {
                     System.out.println("No entry found with the specified ID.");
                 }
-            }
+
+    }
+
+
+    public void deletAccount(String username) throws SQLException {
+
+
+        PreparedStatement pstmt = connection.prepareStatement("DELETE FROM Account WHERE username = ?");
+        pstmt.setString(1, username);
+        int rowsAffected = pstmt.executeUpdate();
+        if (rowsAffected > 0) {
+            System.out.println("entry deleted seccessfully");
+            deleteTable(username);
+        } else {
+            System.out.println("No entry found with the specified ID.");
+
         }
     }
 
-    public void deletAccount(String username) throws SQLException{
-        String query = "DELETE FROM Account WHERE username = ?";
-
-        try (PreparedStatement pstmt = connection.prepareStatement(query)){
-            pstmt.setString(1,username);
-            int rowsAffected = pstmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("entry deleted seccessfully");
-                deleteTable(username);
-            } else {
-                System.out.println("No entry found with the specified ID.");
-
-            }
-        }
-    }
 
     public void deleteTable(String tableName) throws SQLException {
         // Ensure the table name is safe to use in SQL queries
@@ -166,15 +163,12 @@ public class EntrySql {
         }
 
         String query = "DROP TABLE " + tableName;
-        try (
-             Statement stmt = connection.createStatement()) {
+
+             Statement stmt = connection.createStatement();
             stmt.executeUpdate(query);
             System.out.println("Table " + tableName + " deleted successfully");
-        } catch (SQLException e) {
-            System.out.println("An error occurred: " + e.getMessage());
-            // Handle the exception properly, potentially rethrowing or logging it
-        }
     }
+
 
 
 
